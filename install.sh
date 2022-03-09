619333f0e382c9809cb0e5c24a62b2db839e43b1
huangminhang <mhesat@outlook.com>
2022-03-08 23:36:24 -0800

    fix: 更新配置

--- install.sh
+++ install.sh
@@ -10,4 +10,5 @@ git clone git@github.com:akinsho/bufferline.nvim.git ~/.config/nvim/plugged/buff
 git clone git@github.com:neoclide/coc.nvim.git ~/.config/nvim/plugged/coc.nvim
 git clone git@github.com:luochen1990/rainbow.git ~/.config/nvim/plugged/rainbow  
 git clone git@github.com:junegunn/fzf.vim.git ~/.config/nvim/plugged/fzf.vim
+git clone git@github.com:junegunn/fzf.git  ~/.fzf
 git clone git@github.com:sonph/onehalf.git ~/.config/nvim/plugged/onehalf
--- lua/plugin/gruvbox.lua
+++
@@ -1,3 +0,0 @@
-vim.opt.termguicolors = true
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
