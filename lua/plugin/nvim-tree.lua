bfd846661ea6589ef08cc7c99cf51ed5ac7501fd
minhanghuang <mhesat@outlook.com>
2022-03-30 10:28:09 +0800

    新版本文件树插件删除自动关闭功能

--- lua/plugin/nvim-tree.lua
+++ lua/plugin/nvim-tree.lua
@@ -10,7 +10,7 @@ require'nvim-tree'.setup {
 	-- will not open on setup if the filetype is in this list
 	ignore_ft_on_setup  = {},
 	-- closes neovim automatically when the tree is the last **WINDOW** in the view
-	auto_close          = true,
+	auto_close          = false, -- 新版本插件删除自动关闭功能
 	-- opens the tree when changing/opening a new tab if the tree wasn't previously opened
 	open_on_tab         = true,
 	-- hijacks new directory buffers when they are opened.
