cfa385e28ef982b795c37faec2cfc1526efb04de
minhanghuang <mhesat@outlook.com>
2022-04-21 11:06:39 +0800

    updtae treesitter 与 注释插件冲突

--- lua/plugin/nvim-treesitter.lua
+++ lua/plugin/nvim-treesitter.lua
@@ -14,23 +14,5 @@ require("nvim-treesitter.configs").setup(
             -- 使用 treesitter 高亮而不是 neovim 内置的高亮
             additional_vim_regex_highlighting = false
         },
-        -- 范围选择
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                -- 初始化选择
-                init_selection = "<CR>",
-                -- 递增
-                node_incremental = "<CR>",
-                -- 递减
-                node_decremental = "<BS>",
-                -- 选择一个范围
-                scope_incremental = "<TAB>"
-            }
-        },
-        -- 缩进，关闭
-        indent = {
-            enable = false
-        },
     }
 )
