85891f08dec2b595649ef987a72766dce11b705b
minhanghuang <mhesat@outlook.com>
2022-04-23 15:11:29 +0800

    添加lua脚本

--- lua/options.lua
+++ lua/options.lua
@@ -1,23 +1,25 @@
 local options = {
-    number = true;
-    cursorline = true;
-    shiftwidth = 2;
-    expandtab = true;
-    history = 1024;
+    number = true;            -- set numbered lines
+    cursorline = true;        -- highlight the current line
+    shiftwidth = 2;           -- the number of spaces inserted for each indentation
+    expandtab = true;         -- convert tabs to spaces  
+    termguicolors = true,     -- set term gui colors (most terminals support this)
+    history = 1024; 
     showcmd = true;
     showmatch = true;
     showfulltag = true;
     ruler = true;
     autoread = true;
     hlsearch = true;
-    ignorecase = true;
+    ignorecase = true;         -- ignore case in search patterns
     -- noignorecase = true;
-    cmdheight = 1;
+    cmdheight = 1;             -- more space in the neovim command line for displaying messages
     encoding = "utf-8";
     fileencodings = "utf-8,gbk2312,gbk,gb18030,cp936";
     clipboard = "unnamedplus"; -- allows neovim to access the system clipboard 
 }
 
+vim.opt.shortmess:append "c"
 
 for k, v in pairs(options) do
   vim.opt[k] = v
--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -1,13 +1,34 @@
-require("telescope").setup()
--- 查找文件
-vim.keybinds.gmap("n", "<leader>ff", "<cmd>Telescope find_files theme=dropdown<CR>", vim.keybinds.opts)
--- 查找文字
-vim.keybinds.gmap("n", "<leader>fg", "<cmd>Telescope live_grep theme=dropdown<CR>", vim.keybinds.opts)
--- 查找特殊符号
-vim.keybinds.gmap("n", "<leader>fb", "<cmd>Telescope buffers theme=dropdown<CR>", vim.keybinds.opts)
--- 查找帮助文档
-vim.keybinds.gmap("n", "<leader>fh", "<cmd>Telescope help_tags theme=dropdown<CR>", vim.keybinds.opts)
--- 查找最近打开的文件
-vim.keybinds.gmap("n", "<leader>fo", "<cmd>Telescope oldfiles theme=dropdown<CR>", vim.keybinds.opts)
--- 查找 marks 标记
-vim.keybinds.gmap("n", "<leader>fm", "<cmd>Telescope marks theme=dropdown<CR>", vim.keybinds.opts)
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-j>"] = "move_selection_next",
+        ["<C-k>"] = "move_selection_previous",
+      }
+    },
+    -- other layout configuration here
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+
+    -- find_files = {
+      -- theme = "ivy",
+    -- },
+    live_grep = {
+      theme = "dropdown",
+    }
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
+
