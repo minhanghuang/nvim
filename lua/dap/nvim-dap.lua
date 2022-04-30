a4ce4d3267bd158c95f68641c71190f9002ed11f
minhanghuang <mhesat@outlook.com>
2022-04-30 14:26:25 +0800

    feat: python dap debugger

--- lua/dap/nvim-dap.lua
+++ lua/dap/nvim-dap.lua
@@ -30,16 +30,13 @@ vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
 vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
 vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
 
--- 加载调试器配置
-local dap_config = {
-    -- python = require("dap.python"),
-    cpp = require("dap.cpp")
-}
+local python = require("dap.python")
+local cpp = require("dap.cpp")
 
--- 设置调试器
-for dap_name, dap_options in pairs(dap_config) do
-    dap.adapters.cppdbg = dap_options.adapters
-    dap.configurations.cpp = dap_options.configurations
-    dap.configurations.c = dap.configurations.cpp
-end
+dap.adapters.python = python.adapters
+dap.configurations.python = python.configurations
+
+dap.adapters.cppdbg = dap_options.adapters
+dap.configurations.cpp = dap_options.configurations
+dap.configurations.c = dap.configurations.cpp
 
