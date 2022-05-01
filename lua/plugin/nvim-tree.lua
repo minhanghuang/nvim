f3fa9fa22ba67e6bf19099aabf6083dfa7d3011f
minhanghuang <mhesat@outlook.com>
2022-05-01 10:11:49 +0800

    fix: 去掉coc lua插件 & 更新NvimTree配置

--- lua/plugin/nvim-tree.lua
+++ lua/plugin/nvim-tree.lua
@@ -10,22 +10,22 @@ require'nvim-tree'.setup {
 	-- will not open on setup if the filetype is in this list
 	ignore_ft_on_setup  = {},
 	-- closes neovim automatically when the tree is the last **WINDOW** in the view
-	auto_close          = false, -- 新版本插件删除自动关闭功能
+	-- auto_close          = false, -- 新版本插件删除自动关闭功能
 	-- opens the tree when changing/opening a new tab if the tree wasn't previously opened
 	open_on_tab         = true,
 	-- hijacks new directory buffers when they are opened.
-	update_to_buf_dir   = {
-		-- enable the feature
-		enable = true,
-		-- allow to open the tree if it was previously closed
-		auto_open = true,
-	},
+	-- update_to_buf_dir   = {
+	-- 	-- enable the feature
+	-- 	enable = true,
+	-- 	-- allow to open the tree if it was previously closed
+	-- 	auto_open = true,
+	-- },
 	-- hijack the cursor in the tree to put it at the start of the filename
 	hijack_cursor       = false,
 	-- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
 	update_cwd          = false,
 	-- show lsp diagnostics in the signcolumn
-	lsp_diagnostics     = false,
+	-- lsp_diagnostics     = false,
 	-- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
 	update_focused_file = {
 		-- enables the feature
@@ -58,7 +58,7 @@ require'nvim-tree'.setup {
 		-- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
 		side = 'left',
 		-- if true the tree will resize itself after opening a file
-		auto_resize = true,
+		-- auto_resize = true,
 		mappings = {
 			-- custom only false will merge the list with the default mappings
 			-- if true, it will only use your list to set the mappings
