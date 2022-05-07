fd6ba3e448461eda18b582d77875b3b0e4c66223
minhanghuang <mhesat@outlook.com>
2022-05-07 22:40:47 +0800

    style: commentary

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,31 +1,31 @@
--- 主题 
+--  主题 
 require("plugin/gruvbox")
 -- require("plugin/github-nvim-theme")
 -- require("plugin/catppuccin")
  
--- 搜索 
+--  搜索 
 require("plugin/telescope")
  
---  插件: buffers窗口 
+--  buffers窗口 
 require("plugin/bufferline")
 
--- debugger 
+--  debugger 
 require("dap/dap-init")
 
---  插件: 文件树
+--  文件树
 require("plugin/nvim-tree")
 require("plugin/nvim-web-devicons")
 
---  插件: 搜索美化 
+--  搜索美化 
 require("plugin/nvim-hlslens")
  
---  插件: 语法高亮
+--  语法高亮
 require("plugin/nvim-treesitter")
  
---  插件: 光标会停留在上次编辑时的位置 
+--  光标会停留在上次编辑时的位置 
 require("plugin/nvim-lastplace")
 
---  插件: 显示tab缩进 
+--  显示tab缩进 
 require("plugin/indent-blankline")
  
 -- 状态栏美化  
