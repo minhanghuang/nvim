6dd5aa4d11e99a9ca628545619011fe465bb514a
minhanghuang <mhesat@outlook.com>
2022-05-09 11:51:35 +0800

    feat: telescope 视图区新增上下滚动快捷键

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -1,14 +1,18 @@
+local actions = require "telescope.actions"
+
 require('telescope').setup{
   defaults = {
     -- Default configuration for telescope goes here:
     -- config_key = value,
     mappings = {
       i = {
-        -- map actions.which_key to <C-h> (default: <C-/>)
-        -- actions.which_key shows the mappings for your picker,
-        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        ["<C-j>"] = "move_selection_next",
-        ["<C-k>"] = "move_selection_previous",
+        -- 结果窗口 向上/下选择 
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        
+        -- 预览窗口 向上/下滚动
+        ["<C-u>"] = actions.preview_scrolling_up,
+        ["<C-d>"] = actions.preview_scrolling_down,
       }
     },
     -- other layout configuration here
