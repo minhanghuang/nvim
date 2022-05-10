e031fb7cd98d91b9d07116fa3ee54f45934130bd
minhanghuang <mhesat@outlook.com>
2022-05-10 20:56:58 +0800

    feat: 删除coc-git, 更新gitsigns

--- lua/plugin/coc.lua
+++ lua/plugin/coc.lua
@@ -1,3 +1,3 @@
 vim.cmd [[
-    let g:coc_global_extensions = ['coc-json','coc-css','coc-clangd','coc-cmake','coc-git','coc-yaml','coc-sh','coc-highlight','coc-markdownlint','coc-snippets','coc-emmet','coc-html','coc-jedi']
+    let g:coc_global_extensions = ['coc-json','coc-css','coc-clangd','coc-cmake','coc-yaml','coc-sh','coc-highlight','coc-markdownlint','coc-snippets','coc-emmet','coc-html','coc-jedi']
 ]]
