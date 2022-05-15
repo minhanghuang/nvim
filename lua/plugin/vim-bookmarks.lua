ffa4a1d38bb9014ccee0342a94003655d77b1499
minhanghuang <mhesat@outlook.com>
2022-05-15 13:16:21 +0800

    update

--- lua/plugin/vim-bookmarks.lua
+++ lua/plugin/vim-bookmarks.lua
@@ -1,4 +1,4 @@
 vim.g.bookmark_sign = '' 
-let g:bookmark_annotation_sign = ''
+vim.g.bookmark_annotation_sign = ''
 vim.g.bookmark_highlight_lines = 1 
 vim.g.bookmark_no_default_key_mappings = 1 
