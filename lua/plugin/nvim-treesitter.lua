e99e296f19b4280d8d3fa6d70711eaede1e2b5be
minhanghuang <mhesat@outlook.com>
2022-05-16 14:59:52 +0800

    feat: 修改快捷键设置, 保存&增量选择

--- lua/plugin/nvim-treesitter.lua
+++ lua/plugin/nvim-treesitter.lua
@@ -17,8 +17,8 @@ require("nvim-treesitter.configs").setup( {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = '<CR>',
-          node_incremental = '<CR>',
+          init_selection = '<Leader>[',
+          node_incremental = '<Leader>[',
           node_decremental = '<BS>',
           scope_incremental = '<TAB>',
         }
