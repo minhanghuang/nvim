8e49387b7885b980ab4ae6a063ff031472ad5f25
minhanghuang <mhesat@outlook.com>
2022-05-16 09:57:22 +0800

    feat: 代码块增量选择

--- lua/plugin/nvim-treesitter.lua
+++ lua/plugin/nvim-treesitter.lua
@@ -1,7 +1,6 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 
-require("nvim-treesitter.configs").setup(
-    {
+require("nvim-treesitter.configs").setup( {
         -- 安装的高亮支持来源
         -- ensure_installed = "maintained",
         ensure_installed = { "c", "cpp", "python", "bash", "cmake", "lua", "css", "json", "javascript", "make", "vue", "yaml", "vim", "html"}, 
@@ -14,5 +13,15 @@ require("nvim-treesitter.configs").setup(
             -- 使用 treesitter 高亮而不是 neovim 内置的高亮
             additional_vim_regex_highlighting = false
         },
+        -- 启用增量选择
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<CR>',
+          node_incremental = '<CR>',
+          node_decremental = '<BS>',
+          scope_incremental = '<TAB>',
+        }
+      },
     }
 )
