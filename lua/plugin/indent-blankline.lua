132b95dffffe706dc489e77669756296d289344b
minhanghuang <mhesat@outlook.com>
2022-06-13 18:58:02 +0800

    feat: 缩进不显示字符

--- lua/plugin/indent-blankline.lua
+++ lua/plugin/indent-blankline.lua
@@ -1,7 +1,7 @@
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 
 vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
+-- vim.opt.listchars:append("space:⋅")
 -- vim.opt.listchars:append("eol:↴")
 
 require("indent_blankline").setup {
