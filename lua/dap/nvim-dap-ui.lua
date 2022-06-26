69f336a8ca2df56213fb8f023435b5fd46775938
minhanghuang <mhesat@outlook.com>
2022-06-26 17:55:04 +0800

    feat: dap-ui库更新

--- lua/dap/nvim-dap-ui.lua
+++ lua/dap/nvim-dap-ui.lua
@@ -13,42 +13,54 @@ dapui.setup(
             repl = "r",
             toggle = "t"
         },
-        -- 侧边栏
-        sidebar = {
+        -- Expand lines larger than the window
+        -- Requires >= 0.7
+        expand_lines = vim.fn.has("nvim-0.7"),
+        -- Layouts define sections of the screen to place windows.
+        -- The position can be "left", "right", "top" or "bottom".
+        -- The size specifies the height/width depending on position.
+        -- Elements are the elements shown in the layout (in order).
+        -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+        layouts = {
+          {
             elements = {
-                {
-                    id = "scopes",
-                    size = 0.25 -- Can be float or integer > 1
-                },
-                {id = "breakpoints", size = 0.25},
-                {id = "stacks", size = 0.25},
-                {id = "watches", size = 00.25}
+            -- Elements can be strings or table with id and size keys.
+              { id = "scopes", size = 0.25 },
+              "breakpoints",
+              "stacks",
+              "watches",
             },
             size = 40,
-            position = "right"
-        },
-        -- 悬浮窗口
-        floating = {
-            max_height = nil, -- These can be integers or a float between 0 and 1.
-            max_width = nil, -- Floats will be treated as percentage of your screen.
-            border = "single", -- Border style. Can be "single", "double" or "rounded"
-            mappings = {
-                close = {"q", "<Esc>"}
-            }
-        },
-        -- codelldb 运行窗口
-        tray = {
-            elements = {"repl"},
+            position = "right",
+          },
+          {
+            elements = {
+              -- "repl",
+              "console",
+            },
             size = 10,
-            position = "bottom" -- Can be "left", "right", "top", "bottom"
+            position = "bottom",
+          },
+        },
+        floating = {
+          max_height = nil, -- These can be integers or a float between 0 and 1.
+          max_width = nil, -- Floats will be treated as percentage of your screen.
+          border = "single", -- Border style. Can be "single", "double" or "rounded"
+          mappings = {
+            close = { "q", "<Esc>" },
+          },
+        },
+        windows = { indent = 1 },
+        render = {
+          max_type_length = nil, -- Can be integer or nil.
         }
-    }
+      }
 )
 -- 如果开启或关闭调试，则自动打开或关闭调试界面
 dap.listeners.after.event_initialized["dapui_config"] = function()
     -- 不开codelldb窗口
-    -- dapui.open()
-    dapui.open("sidebar")
+    dapui.open()
+    -- dapui.open("sidebar")
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
     dapui.close()
