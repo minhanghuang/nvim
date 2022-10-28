540ed28242b267cadb9fa99a86a7d15fe8832b8e
minhanghuang <mhesat@outlook.com>
2022-10-28 12:04:39 +0800

    feat: 当前文件搜索光标所在字符串

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -167,12 +167,20 @@ keymap("n",
 )
 
 -- 搜索(telescope) 
+function _G.grep_string_the_file()
+  local telescope = require('telescope.builtin')
+  require('telescope.builtin').grep_string({
+      grep_open_files = true
+  })
+end
 -- 搜索文件 
 keymap("n", "<Leader>ff", "<cmd>Telescope find_files<cr>", opts)
 -- 搜索字符串(cwd, 项目根目录)
 keymap("n", "<Leader>fs", "<cmd>Telescope live_grep<cr>", opts)
--- 搜索光标所在字符串
+-- 搜索光标所在字符串(所有文件)
 keymap("n", "<Leader>fh", "<cmd>Telescope grep_string<cr>", opts)
+-- 搜索光标所在字符串(当前文件)
+keymap("n", "<Leader>fc", "<cmd>lua _G.grep_string_the_file()<cr>", opts)
 -- 搜索TODO LIST 
 keymap("n", "<Leader>ft", "<cmd>TodoTelescope<cr>", opts)
 
