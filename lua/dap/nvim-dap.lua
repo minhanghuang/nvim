d3658e9ba69f51580da6eb72f4a20e4c28a73edf
minhanghuang <job@minhang.me>
2023-01-12 10:37:03 +0800

    fix: 解决dap连不上服务器问题#https://github.com/mfussenegger/nvim-dap/issues/717

--- lua/dap/nvim-dap.lua
+++ lua/dap/nvim-dap.lua
@@ -28,6 +28,11 @@ vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
 vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
 vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
 
+-- 解决: https://github.com/mfussenegger/nvim-dap/issues/717
+vim.cmd[[
+  language en_US.UTF-8
+]]
+
 require("dap.dap-python")
 require('dap.dap-cpp-codelldb')
 
