b6f1e9f428fe46c9d0279f5eee8994c9e5d5b590
minhanghuang <job@minhang.me>
2023-11-20 16:21:17 +0800

    revert: 回退snippets插件

--- lua/user/conf/init.lua
+++ lua/user/conf/init.lua
@@ -51,7 +51,29 @@ return {
     "neoclide/coc.nvim",
     tag = "v0.0.81",
     config = function()
-      require("user.conf.coc")
+      vim.g.coc_global_extensions = {
+        'coc-vetur', -- vue
+        'coc-sumneko-lua@0.0.30',
+        'coc-json',
+        'coc-css@1.3.0',
+        'coc-clangd@0.25.0',
+        'coc-cmake',
+        'coc-yaml',
+        'coc-sh@1.0.0',
+        'coc-highlight@1.3.0',
+        'coc-markdownlint',
+        'coc-emmet',
+        'coc-html',
+        'coc-pyright',
+        'coc-translator',
+        'coc-snippets',
+        'coc-xml',
+        'coc-spell-checker', -- 单词校验
+      }
+
+      -- <C-j> <C-k> 冲突
+      vim.g.coc_snippet_prev = '<S-j>'
+      vim.g.coc_snippet_next = '<S-k>'
     end,
   },
 
@@ -86,22 +108,22 @@ return {
     end,
   },
 
-  {
-    "L3MON4D3/LuaSnip",
-    version = "2.*",
-    dependencies = { "rafamadriz/friendly-snippets" },
-    event = "VeryLazy",
-    config = function()
-      require("luasnip").setup({
-        history = true,
-        delete_check_events = "TextChanged,TextChangedI",
-      })
-      require("luasnip.loaders.from_vscode").lazy_load()
-      -- 自定义代码片段需要将`endsnippet`删除并缩进一级(https://github.com/L3MON4D3/LuaSnip/wiki/Migrating-from-UltiSnips#snippets)
-      -- 这将和coc.snippets冲突
-      require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
-    end,
-  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   version = "2.*",
+  --   dependencies = { "rafamadriz/friendly-snippets" },
+  --   event = "VeryLazy",
+  --   config = function()
+  --     require("luasnip").setup({
+  --       history = true,
+  --       delete_check_events = "TextChanged,TextChangedI",
+  --     })
+  --     require("luasnip.loaders.from_vscode").lazy_load()
+  --     -- 自定义代码片段需要将`endsnippet`删除并缩进一级(https://github.com/L3MON4D3/LuaSnip/wiki/Migrating-from-UltiSnips#snippets)
+  --     -- 这将和coc.snippets冲突
+  --     require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+  --   end,
+  -- },
 
   -- 添加函数接口说明
   {
