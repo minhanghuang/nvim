6b46243909432292470d2e6720bbabb7fbaa63e8
minhanghuang <job@minhang.me>
2023-11-26 14:50:41 +0800

    feat(plugin): 增加mason安装工具插件

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,10 +1,13 @@
 return {
   -- LSP
   {
-    "neovim/nvim-lspconfig",               -- lsp配置
+    "neovim/nvim-lspconfig",                         -- lsp配置
     dependencies = {
-      "williamboman/mason.nvim",           -- 管理lsp插件
-      "williamboman/mason-lspconfig.nvim", -- 配置nvim-lspconfig
+      "williamboman/mason.nvim",                     -- 管理lsp插件
+      "williamboman/mason-lspconfig.nvim",           -- 配置nvim-lspconfig
+      {
+        "WhoIsSethDaniel/mason-tool-installer.nvim", -- 安装工具(解决一些非lsp工具mason-lspconfig.nvim无法安装问题)
+      },
       {
         lazy = true,
         "b0o/schemastore.nvim", -- json schemas
