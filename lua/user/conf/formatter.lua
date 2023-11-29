f63c3b30ab41c66cdfb90bebc1094a9816b51411
minhanghuang <job@minhang.me>
2023-11-29 10:05:03 +0800

    perf(formatting): 更新formater插件配置, 增加默认配置

--- lua/user/conf/formatter.lua
+++ lua/user/conf/formatter.lua
@@ -16,6 +16,13 @@ formatter.setup {
           stdin = false
         }
       end
+    },
+    -- Use the special "*" filetype for defining formatter configurations on
+    -- any filetype
+    ["*"] = {
+      -- "formatter.filetypes.any" defines default configurations for any
+      -- filetype
+      require("formatter.filetypes.any").remove_trailing_whitespace
     }
   }
 }
