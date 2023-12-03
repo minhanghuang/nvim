3fa2ff0203c79e97c62ee0c50271969d35c40588
minhanghuang <job@minhang.me>
2023-12-03 12:47:37 +0800

    feat(telesscope): 增加lsp code-action UI

--- lua/user/conf/telescope.lua
+++ lua/user/conf/telescope.lua
@@ -38,5 +38,28 @@ telescope.setup {
     --   extension_config_key = value,
     -- }
     -- please take a look at the readme of the extension you want to configure
+
+
+    -- lsp code-action
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown {
+        -- even more opts
+      },
+      -- pseudo code / specification for writing custom displays, like the one
+      -- for "codeactions"
+      -- specific_opts = {
+      --   [kind] = {
+      --     make_indexed = function(items) -> indexed_items, width,
+      --     make_displayer = function(widths) -> displayer
+      --     make_display = function(displayer) -> function(e)
+      --     make_ordinal = function(e) -> string
+      --   },
+      --   -- for example to disable the custom builtin "codeactions" display
+      --      do the following
+      --   codeactions = false,
+      -- }
+    }
   }
 }
+
+require("telescope").load_extension("ui-select")
