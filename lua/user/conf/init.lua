3897f591ddb64476b78df775ab5e0e1eca9d0415
minhanghuang <job@minhang.me>
2023-12-16 10:50:23 +0800

    feat(plugin): 增加显示代码上下文插件

--- lua/user/conf/init.lua
+++ lua/user/conf/init.lua
@@ -160,6 +160,15 @@ return {
     end,
   },
 
+  -- 显示代码上下文
+  {
+    -- https://github.com/nvim-treesitter/nvim-treesitter-context
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require("user.conf.nvim-treesitter-context")
+    end,
+  },
+
   -- icon
   {
     "nvim-tree/nvim-web-devicons",
