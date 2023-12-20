f77d4e1fe5167d78691eb85a4e71b9efe0d29363
minhanghuang <job@minhang.me>
2023-12-20 10:21:43 +0800

    perf(cmp): 增加clangd拓展插件优化补全功能

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -31,6 +31,12 @@ return {
       --   config = function()
       --   end
       -- }
+
+      -- clang拓展插件
+      {
+        -- https://github.com/p00f/clangd_extensions.nvim
+        "p00f/clangd_extensions.nvim",
+      }
     },
     config = function()
       local luasnip = require("luasnip")
@@ -157,17 +163,18 @@ return {
 
         sorting = {
           priority_weight = 2,
-          comparators = {                     -- 定义了多个比较函数，用于确定补全项的顺序。这些函数按照列表中的顺序依次应用
-            -- deprioritize_snippet,             -- 降低代码片段 (snippets) 的优先级
-            cmp.config.compare.locality,      -- 局部
-            cmp.config.compare.offset,        -- 补全项在文件中的位置（偏移量）进行排序
-            cmp.config.compare.exact,         -- 补全项与用户输入的文本的匹配度进行排序
-            cmp.config.compare.score,         -- 补全项的得分进行排序
-            cmp.config.compare.recently_used, -- 最近使用
-            cmp.config.compare.kind,          -- 补全项的数据类型进行排序
-            cmp.config.compare.sort_text,     -- 补全项的排序文本进行排序
-            cmp.config.compare.length,        -- 补全项的长度进行排序
-            cmp.config.compare.order,         -- 插件内部的预定义顺序对补全项进行排序
+          comparators = {                            -- 定义了多个比较函数，用于确定补全项的顺序。这些函数按照列表中的顺序依次应用
+            -- deprioritize_snippet,                 -- 降低代码片段 (snippets) 的优先级
+            require("clangd_extensions.cmp_scores"), -- clangd extensions tools
+            cmp.config.compare.locality,             -- 局部
+            cmp.config.compare.offset,               -- 补全项在文件中的位置（偏移量）进行排序
+            cmp.config.compare.exact,                -- 补全项与用户输入的文本的匹配度进行排序
+            cmp.config.compare.score,                -- 补全项的得分进行排序
+            cmp.config.compare.recently_used,        -- 最近使用
+            cmp.config.compare.kind,                 -- 补全项的数据类型进行排序
+            cmp.config.compare.sort_text,            -- 补全项的排序文本进行排序
+            cmp.config.compare.length,               -- 补全项的长度进行排序
+            cmp.config.compare.order,                -- 插件内部的预定义顺序对补全项进行排序
           },
         },
 
