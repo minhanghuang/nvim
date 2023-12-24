4374bd806d5a7e342cbe284e021e0db0cd9651c9
minhanghuang <job@minhang.me>
2023-12-24 11:25:38 +0800

    refactor(lsp): 调整目录结构

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -16,9 +16,6 @@ return {
     opts = {
       -- options for vim.diagnostic.config()
       diagnostics = {
-        -- signs = {
-        --   active = signs,
-        -- },
         underline = true, -- 将代码中诊断出的问题用下划线标记
         update_in_insert = false, -- 控制是否在插入模式中更新诊断信息
         severity_sort = true, -- 控制是否按照诊断的严重程度对其进行排序
@@ -36,10 +33,76 @@ return {
           prefix = "●", -- 定义虚拟文本前缀
         },
       },
-      -- diagnostics
     },
     config = function()
-      require("user.lsp.handlers").setup()
+      local lspconfig = require("lspconfig")
+      local mason = require("mason")
+      local mason_config = require("mason-lspconfig")
+      local mason_tool_installer = require("mason-tool-installer")
+
+      -- diagnostics signs
+      for name, icon in pairs(require("user.config").defaults.icons.diagnostics) do
+        name = "DiagnosticSign" .. name
+        vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+      end
+
+      mason.setup({
+        ui = {
+          icons = require("user.config").defaults.icons.mason
+        }
+      })
+
+      -- :Mason 查看lsp server状态
+      -- :LspLog 查看lsp日志
+      mason_config.setup {
+        -- Node.js version: v15.4.0
+        -- 安装列表: https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
+        ensure_installed = {
+          'lua_ls',
+          'pyright',
+          'clangd',
+          'html@4.0.0',
+          'cmake',                           -- dependence python3-venv
+          'jsonls@4.0.0',                    -- json
+          'yamlls',                          -- yaml
+          'bashls@4.0.0',                    -- bash
+          'vls',                             -- vue.js
+          'docker_compose_language_service', -- docker-compose
+          'dockerls',                        -- docker file
+        },
+      }
+
+      mason_tool_installer.setup({
+        ensure_installed = {
+          -- format
+          "autopep8", -- python formatter
+          "black",    -- python formatter
+          "prettier", -- Angular, CSS, Flow, GraphQL, HTML, JSON, JSX, JavaScript, LESS, Markdown, SCSS, TypeScript, Vue, YAML
+        },
+      })
+
+      -- { key: 服务器名, value: 配置文件 }
+      local servers = {
+        jsonls = require("user.lsp.providers.jsonls"),
+        lua_ls = require("user.lsp.providers.sumneko_lua"),
+        pyright = require("user.lsp.providers.pyright"),
+        clangd = require("user.lsp.providers.clangd"),
+        html = require("user.lsp.providers.html"),
+        cmake = require("user.lsp.providers.cmake"),
+        yamlls = require("user.lsp.providers.yamlls"),
+        bashls = require("user.lsp.providers.bashls"),
+        vuels = require("user.lsp.providers.vue"),
+        docker_compose_language_service = require("user.lsp.providers.docker-compose"),
+        dockerls = require("user.lsp.providers.default"),
+      }
+
+      for name, config in pairs(servers) do
+        if config then
+          lspconfig[name].setup(config)
+        else
+          lspconfig[name].setup({}) -- 默认配置
+        end
+      end
     end,
   },
 }
