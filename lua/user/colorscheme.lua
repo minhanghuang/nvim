7e8903617b703bc4c3919d2d2030979cec150fd9
minhanghuang <job@minhang.me>
2023-12-26 10:33:09 +0800

    feat: 增加主题

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,12 +1,31 @@
 return {
   {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    branch = "main",
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+  },
+  {
     "ellisonleao/gruvbox.nvim",
+    lazy = false,
     commit = "353be59",
     name = "gruvbox",
-    lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd("colorscheme gruvbox")
+      local config = require("user.config")
+      require("user.themes.tokyonight")
+      require("user.themes.catppuccin")
+      require("user.themes.gruvbox")
+      local theme_ok, _ = pcall(vim.cmd, "colorscheme " .. config.defaults.theme)
+      if not theme_ok then
+        vim.notify("colorscheme " .. config.defaults.theme .. " not found!")
+        return
+      end
     end,
   }
 }
---
+++ lua/user/themes/catppuccin.lua
@@ -0,0 +1,43 @@
+local has_catppuccin, catppuccin = pcall(require, "catppuccin")
+if not has_catppuccin then
+  vim.notify("catppuccin.nvim not found!")
+  return
+end
+
+catppuccin.setup({
+  integrations = {
+    aerial = true,
+    alpha = true,
+    cmp = true,
+    dashboard = true,
+    flash = true,
+    gitsigns = true,
+    headlines = true,
+    illuminate = true,
+    indent_blankline = { enabled = true },
+    leap = true,
+    lsp_trouble = true,
+    mason = true,
+    markdown = true,
+    mini = true,
+    native_lsp = {
+      enabled = true,
+      underlines = {
+        errors = { "undercurl" },
+        hints = { "undercurl" },
+        warnings = { "undercurl" },
+        information = { "undercurl" },
+      },
+    },
+    navic = { enabled = true, custom_bg = "lualine" },
+    neotest = true,
+    neotree = true,
+    noice = true,
+    notify = true,
+    semantic_tokens = true,
+    telescope = true,
+    treesitter = true,
+    treesitter_context = true,
+    which_key = true,
+  },
+})
