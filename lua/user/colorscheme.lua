3e5b895e323b07ca52268fb389bc9a9637dd2955
minhanghuang <job@minhang.me>
2023-12-26 10:43:05 +0800

    refactor: 调整配置文件

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -17,13 +17,13 @@ return {
     name = "gruvbox",
     priority = 1000,
     config = function()
-      local config = require("user.config")
+      local g_config = require("user.config")
       require("user.themes.tokyonight")
       require("user.themes.catppuccin")
       require("user.themes.gruvbox")
-      local theme_ok, _ = pcall(vim.cmd, "colorscheme " .. config.defaults.theme)
+      local theme_ok, _ = pcall(vim.cmd, "colorscheme " .. g_config.defaults.theme)
       if not theme_ok then
-        vim.notify("colorscheme " .. config.defaults.theme .. " not found!")
+        vim.notify("colorscheme " .. g_config.defaults.theme .. " not found!")
         return
       end
     end,
