68d285469437bad64ca3aeabbe01facf660f4537
minhanghuang <job@minhang.me>
2023-12-30 11:12:36 +0800

    feat(lsp): html增加对ros launch的支持

--- lua/user/lsp/providers/html.lua
+++ lua/user/lsp/providers/html.lua
@@ -3,4 +3,5 @@ local config = require("user.lsp.config")
 return {
   capabilities = config.capabilities,
   on_attach = config.on_attach,
+  filetypes = { "html", "launch" }, -- add Ros launch filetype
 }
