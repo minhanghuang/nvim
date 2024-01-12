cac9fd47c7bd712d5a8811b27fdef082eef1e62d
minhanghuang <job@minhang.me>
2024-01-12 09:54:35 +0800

    refactor: 增加util

--- lua/user/conf/nvim-tree.lua
+++ lua/user/conf/nvim-tree.lua
@@ -1,12 +1,11 @@
--- following options are the default
-local telescope = require('telescope.builtin')
-
 local function live_grep_the_node_cb(node)
-  telescope.live_grep({ prompt_title = "Search Path:" .. node.absolute_path, search_dirs = { node.absolute_path } })
+  require('user.util').telescope('live_grep',
+    { prompt_title = "Search Path:" .. node.absolute_path, search_dirs = { node.absolute_path } })
 end
 
 local function find_files_the_node_cb(node)
-  telescope.find_files({ search_dirs = { node.absolute_path } })
+  require('user.util').telescope('find_files',
+    { search_dirs = { node.absolute_path } })
 end
 
 require 'nvim-tree'.setup {
@@ -75,45 +74,45 @@ require 'nvim-tree'.setup {
       -- list of mappings to set on the tree manually
       list = {
         { key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
-        { key = "<C-e>",                        action = "edit_in_place" },
-        { key = { "O" },                        action = "edit_no_picker" },
-        { key = { "<2-RightMouse>", "<C-]>" },  action = "cd" },
-        { key = "<C-v>",                        action = "vsplit" },
-        { key = "<C-x>",                        action = "split" },
-        { key = "<C-t>",                        action = "tabnew" },
-        { key = "<",                            action = "prev_sibling" },
-        { key = ">",                            action = "next_sibling" },
-        { key = "P",                            action = "parent_node" },
-        { key = "<C-o>",                        action = "close_node" },
-        { key = "<Tab>",                        action = "preview" },
-        { key = "K",                            action = "first_sibling" },
-        { key = "J",                            action = "last_sibling" },
-        { key = "I",                            action = "toggle_git_ignored" },
-        { key = "H",                            action = "toggle_dotfiles" },
-        { key = "R",                            action = "refresh" },
-        { key = "a",                            action = "create" },
-        { key = "d",                            action = "remove" },
-        { key = "D",                            action = "trash" },
-        { key = "r",                            action = "rename" },
-        { key = "<C-r>",                        action = "full_rename" },
-        { key = "x",                            action = "cut" },
-        { key = "c",                            action = "copy" },
-        { key = "p",                            action = "paste" },
-        { key = "y",                            action = "copy_name" },
-        { key = "Y",                            action = "copy_path" },
-        { key = "gy",                           action = "copy_absolute_path" },
-        { key = "[c",                           action = "prev_git_item" },
-        { key = "]c",                           action = "next_git_item" },
-        { key = "-",                            action = "dir_up" },
-        { key = "fs",                           action = "live_grep_the_node",  action_cb = live_grep_the_node_cb },
-        { key = "ff",                           action = "find_files_the_node", action_cb = find_files_the_node_cb },
-        { key = "S",                            action = "search_node" },
-        { key = "F",                            action = "clear_live_filter" },
-        { key = "q",                            action = "close" },
-        { key = "g?",                           action = "toggle_help" },
-        { key = "W",                            action = "collapse_all" },
-        { key = "K",                            action = "toggle_file_info" },
-        { key = ".",                            action = "run_file_command" }
+        { key = "<C-e>",                          action = "edit_in_place" },
+        { key = { "O" },                          action = "edit_no_picker" },
+        { key = { "<2-RightMouse>", "<C-]>" },    action = "cd" },
+        { key = "<C-v>",                          action = "vsplit" },
+        { key = "<C-x>",                          action = "split" },
+        { key = "<C-t>",                          action = "tabnew" },
+        { key = "<",                              action = "prev_sibling" },
+        { key = ">",                              action = "next_sibling" },
+        { key = "P",                              action = "parent_node" },
+        { key = "<C-o>",                          action = "close_node" },
+        { key = "<Tab>",                          action = "preview" },
+        { key = "K",                              action = "first_sibling" },
+        { key = "J",                              action = "last_sibling" },
+        { key = "I",                              action = "toggle_git_ignored" },
+        { key = "H",                              action = "toggle_dotfiles" },
+        { key = "R",                              action = "refresh" },
+        { key = "a",                              action = "create" },
+        { key = "d",                              action = "remove" },
+        { key = "D",                              action = "trash" },
+        { key = "r",                              action = "rename" },
+        { key = "<C-r>",                          action = "full_rename" },
+        { key = "x",                              action = "cut" },
+        { key = "c",                              action = "copy" },
+        { key = "p",                              action = "paste" },
+        { key = "y",                              action = "copy_name" },
+        { key = "Y",                              action = "copy_path" },
+        { key = "gy",                             action = "copy_absolute_path" },
+        { key = "[c",                             action = "prev_git_item" },
+        { key = "]c",                             action = "next_git_item" },
+        { key = "-",                              action = "dir_up" },
+        { key = "fs",                             action = "live_grep_the_node",  action_cb = live_grep_the_node_cb },
+        { key = "ff",                             action = "find_files_the_node", action_cb = find_files_the_node_cb },
+        { key = "S",                              action = "search_node" },
+        { key = "F",                              action = "clear_live_filter" },
+        { key = "q",                              action = "close" },
+        { key = "g?",                             action = "toggle_help" },
+        { key = "W",                              action = "collapse_all" },
+        { key = "K",                              action = "toggle_file_info" },
+        { key = ".",                              action = "run_file_command" }
       }
     }
   }
