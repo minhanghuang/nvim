deebd9a6550baa77fe522fbfc733f0d864796f7d
minhanghuang <job@minhang.me>
2024-02-22 09:31:46 +0800

    perf(commentary): 增加proto注释

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -2,7 +2,7 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("user__" .. name, { clear = true })
 end
 
--- 注释c/cpp文件时, 使用//
+-- 注释c/cpp文件, 使用//
 vim.api.nvim_create_autocmd({ "FileType" }, {
   group = augroup("cpp_commentstring"),
   pattern = "c,cpp",
@@ -11,7 +11,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
--- 注释launch文件时, 使用<!-- -->
+-- 注释launch文件, 使用<!-- -->
 vim.api.nvim_create_autocmd({ "FileType" }, {
   group = augroup("launch_commentstring"),
   pattern = "launch",
@@ -20,10 +20,19 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
--- 注释json文件时, 使用//
+-- 注释json文件, 使用//
 vim.api.nvim_create_autocmd({ "FileType" }, {
   group = augroup("json_commentstring"),
   pattern = "json",
+  callback = function()
+    vim.api.nvim_buf_set_option(0, 'commentstring', '// %s')
+  end,
+})
+
+-- 注释proto文件, 使用//
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup("proto_commentstring"),
+  pattern = "proto",
   callback = function()
     vim.api.nvim_buf_set_option(0, 'commentstring', '// %s')
   end,
