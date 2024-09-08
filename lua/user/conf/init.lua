5fea70706076ec45cf4c7707f783d6f009ff1ec5
minhanghuang <job@minhang.me>
2024-09-08 21:10:31 +0800

    feat(plugin): Added markdown view plugin

--- lua/user/conf/init.lua
+++ lua/user/conf/init.lua
@@ -591,6 +591,19 @@ return {
       })
     end,
   },
+  {
+    -- https://github.com/OXY2DEV/markview.nvim
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    ft = "markdown", -- If you decide to lazy-load anyway
+    dependencies = {
+      -- You will not need this if you installed the
+      -- parsers manually
+      -- Or if the parsers are in your $RUNTIMEPATH
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    },
+  },
 
   -- 平滑滚动
   {
