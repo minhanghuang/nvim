1bfcd39431b73a84dd280bd4868e6a9b469d8e18
minhanghuang <job@minhang.me>
2024-09-27 22:02:56 +0800

    feat(plugin): Added leetcode plugin

--- lua/user/conf/init.lua
+++ lua/user/conf/init.lua
@@ -651,6 +651,27 @@ return {
     end,
   },
 
+  -- leetcode
+  {
+    -- https://github.com/kawre/leetcode.nvim
+    -- Usage:
+    --  $ nvim leetcode.nvim
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      -- "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- configuration goes here
+    },
+  },
+
   -- 底部状态栏
   {
     "windwp/windline.nvim",
