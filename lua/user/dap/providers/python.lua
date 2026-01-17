57052764ea6aca233f8c5d1b7a92612d0ef0ea80
minhanghuang <job@minhang.me>
2026-01-17 22:33:27 +0800

    fix(dap): Python dap function call path

--- lua/user/dap/providers/python.lua
+++ lua/user/dap/providers/python.lua
@@ -19,7 +19,7 @@ dap.configurations.python = {
     program = "${file}",
     args = function()
       local input = vim.fn.input("Input args: ")
-      return require("user.dap.dap-util").str2argtable(input)
+      return require("user.dap.util").str2argtable(input)
     end,
     pythonPath = function()
       return "/usr/bin/python3"
