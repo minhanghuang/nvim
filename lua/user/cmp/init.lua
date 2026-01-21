e6d596e7c692475aa07654f595782f0ae1931a4c
minhanghuang <job@minhang.me>
2026-01-21 22:56:57 +0800

    chore(ai): update copliot config

--- lua/user/cmp/init.lua
+++ lua/user/cmp/init.lua
@@ -53,12 +53,13 @@ return {
         "p00f/clangd_extensions.nvim",
       },
 
-      -- ai
+      -- AI
       {
         -- https://github.com/Exafunction/codeium.nvim
         -- 添加代理报错: https://github.com/Exafunction/codeium.nvim/issues/164
         -- export no_proxy=127.0.0.1
         "Exafunction/codeium.nvim",
+        enabled = true,
         cmd = "Codeium",
         build = ":Codeium Auth",
         dependencies = {
@@ -73,8 +74,19 @@ return {
       {
         -- https://github.com/zbirenbaum/copilot.lua
         "zbirenbaum/copilot.lua",
+        enabled = true,
         cmd = { "Copilot", "Copilot Auth" },
         event = "InsertEnter",
+        dependencies = {
+          {
+            -- https://github.com/zbirenbaum/copilot-cmp
+            "zbirenbaum/copilot-cmp",
+            after = { "copilot.lua", "nvim-cmp" },
+            config = function()
+              require("copilot_cmp").setup()
+            end
+          },
+        },
         config = function()
           require("copilot").setup({
             suggestion = { enabled = false },
@@ -82,14 +94,6 @@ return {
           })
         end
       },
-      {
-        -- https://github.com/zbirenbaum/copilot-cmp
-        "zbirenbaum/copilot-cmp",
-        after = { "copilot.lua", "nvim-cmp" },
-        config = function()
-          require("copilot_cmp").setup()
-        end
-      }
     },
     config = function()
       local autopairs = require("nvim-autopairs.completion.cmp")
