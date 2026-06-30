26c4025863334dd2387e109a91388cb601ee552c
minhanghuang <job@minhang.me>
2026-06-30 21:39:54 +0800

    refactor(theme): adjust theme config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -12,7 +12,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  require('user.colorscheme'),
+  require('user.themes'),
   require('user.conf'),
   require('user.lsp'),
   require('user.cmp'),
