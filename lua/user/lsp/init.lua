4610e9033a9fbf53ae2b2ddb681fb5f116fd79b9
minhanghuang <job@minhang.me>
2026-09-15 15:31:32 +0900

    feat(lsp): Added Golang LSP configuration

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -94,6 +94,7 @@ return {
         pylsp = require("user.lsp.providers.pylsp"),
 
         clangd = require("user.lsp.providers.clangd"),
+        gopls = require("user.lsp.providers.default"),
         html = require("user.lsp.providers.html"),
         ts_ls = require("user.lsp.providers.typescript"),
         cmake = require("user.lsp.providers.cmake"),
