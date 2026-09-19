cfe46ce95c6dfe5f56f20ab351d323dcdb1d915f
minhanghuang <job@minhang.me>
2026-09-19 15:33:47 +0900

    feat(dap): Added Golang DAP configration

--- lua/user/config.lua
+++ lua/user/config.lua
@@ -83,6 +83,7 @@ local defaults = {
     dap = {
       { "debugpy",  version = "1.8.0" },   -- Python
       { "codelldb", version = "v1.12.2" }, -- C/C++/Rust
+      { "delve",    version = "v1.27.2" }, -- Golang
     },
   },
 
